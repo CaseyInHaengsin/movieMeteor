cf815653348ebf89028cd638ce5a98ce17afe1ee
CaseyInHaengsin <casey.rowley89@gmail.com>
2018-03-13 06:55:44 -0600

    adding basic functionality on front-end

--- client/main.js
+++ client/main.js
@@ -1,22 +1,23 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Meteor} from 'meteor/meteor';
 
-import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
-});
+Meteor.startup(() => {
 
-Template.hello.helpers({
-  counter() {
-    return Template.instance().counter.get();
-  },
-});
+    let title = 'Did ths work?!';
 
-Template.hello.events({
-  'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
-  },
+    let jsx = (
+
+        <div>
+            <form>
+                <input type='text' name='movieTitle' placeholder='Search for Movie'/>
+                <button>Search Movie</button>
+            </form>
+        </div>
+
+    );
+
+    ReactDOM.render(jsx, document.getElementById('app'));
+
 });
